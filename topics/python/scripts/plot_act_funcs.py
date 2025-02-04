dc4c77a19f49a819e46c52d050fb19f8b1f4a930
jakelevi1996 <jakelevi@hotmail.co.uk>
2025-02-04 13:22:06 +0000

    Fix typo

--- topics/python/scripts/plot_act_funcs.py
+++ topics/python/scripts/plot_act_funcs.py
@@ -5,7 +5,7 @@ x = np.linspace(-10, 10, 500)
 lines = [
     plotting.Line(x, np.abs(x),             label="abs"),
     plotting.Line(x, np.tanh(x) * x,        label="tanhglu"),
-    plotting.Line(x, x / (1 + np.exp(-x)),  label="swiglu"),
+    plotting.Line(x, x / (1 + np.exp(-x)),  label="swish"),
     plotting.Line(x, np.log1p(np.exp(x)),   label="softplus"),
 ]
 plotting.ColourPicker.from_colourise(lines)
