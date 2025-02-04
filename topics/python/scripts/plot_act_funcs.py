0380d2ca839aafbbd345b65c383b313c3f0248a1
jakelevi1996 <jakelevi@hotmail.co.uk>
2025-02-04 13:19:33 +0000

    Add `topics/python/scripts/plot_act_funcs.py`

---
+++ topics/python/scripts/plot_act_funcs.py
@@ -0,0 +1,18 @@
+import numpy as np
+from jutility import plotting
+
+x = np.linspace(-10, 10, 500)
+lines = [
+    plotting.Line(x, np.abs(x),             label="abs"),
+    plotting.Line(x, np.tanh(x) * x,        label="tanhglu"),
+    plotting.Line(x, x / (1 + np.exp(-x)),  label="swiglu"),
+    plotting.Line(x, np.log1p(np.exp(x)),   label="softplus"),
+]
+plotting.ColourPicker.from_colourise(lines)
+plotting.plot(
+    *lines,
+    plotting.Legend(),
+    ylim=[-1, 4],
+    xlim=[-8, 5],
+    figsize=[6, 4],
+)
