07cc15dc406e8658b5232480a49184e1fe2adf5b
jakelevi1996 <jakelevi@hotmail.co.uk>
2025-05-13 22:11:02 +0100

    Add `topics/python/scripts/extract_upper_triangular.py`

---
+++ topics/python/scripts/extract_upper_triangular.py
@@ -0,0 +1,18 @@
+import torch
+import juml
+
+juml.test_utils.torch_set_print_options()
+
+n = 7
+
+x = torch.arange(n*n).reshape(n, n) + 1.0
+print(x)
+
+rc = [
+    [row, row + col]
+    for row in range(n)
+    for col in range(n - row)
+]
+r, c = zip(*rc)
+
+print(x[r, c])
